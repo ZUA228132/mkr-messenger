f2e0db8d68b88a92c1adfa4c1b17110e2013faf5
Makar <makar@fuek.ru>
2026-01-16 12:59:00 +0300

    Remove Firebase from AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import Flutter
 import UIKit
-import FirebaseCore
-import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,17 +7,6 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Initialize Firebase
-    // Requirements: 8.4 - Use APNs for push notifications
-    FirebaseApp.configure()
-    
-    // Set up push notifications
-    UNUserNotificationCenter.current().delegate = self
-    Messaging.messaging().delegate = self
-    
-    // Register for remote notifications
-    application.registerForRemoteNotifications()
-    
     GeneratedPluginRegistrant.register(with: self)
     
     // Set up method channel for app icon management
@@ -71,39 +58,5 @@ import FirebaseMessaging
     }
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-  
-  // MARK: - APNs Token Registration
-  
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    // Pass device token to Firebase
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-  
-  override func application(
-    _ application: UIApplication,
-    didFailToRegisterForRemoteNotificationsWithError error: Error
-  ) {
-    print("Failed to register for remote notifications: \(error.localizedDescription)")
-  }
-}
-
-// MARK: - MessagingDelegate
-
-extension AppDelegate: MessagingDelegate {
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("FCM Token: \(fcmToken ?? "nil")")
-    
-    // Send token to Flutter
-    let dataDict: [String: String] = ["token": fcmToken ?? ""]
-    NotificationCenter.default.post(
-      name: Notification.Name("FCMToken"),
-      object: nil,
-      userInfo: dataDict
-    )
   }
 }
